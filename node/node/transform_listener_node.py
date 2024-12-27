5feaab9e63ab721652199a48d9dcd595f93e4b32
Duy <108132255+ThaiThanhDuy@users.noreply.github.com>
2024-12-27 10:55:32 +0700

    Update transform_listener_node.py

--- node/node/transform_listener_node.py
+++ node/node/transform_listener_node.py
@@ -157,6 +157,7 @@ class TransformListenerNode(Node):
                     angular_z = 0.0
                     self.send_command(linear_x, linear_y, angular_z)  # Send stop command
                     if self.distances['front']['static'] > 1.0 and self.distances['Northeast']['static'] > 1.0:
+                        self.get_logger().info("NO Obstacle F and NE detected !")
                         return  # Exit the function to prevent further movement
                   
                 elif self.distances['right']['static'] < 1.5:
@@ -166,15 +167,16 @@ class TransformListenerNode(Node):
                     angular_z = 0.0
                     self.send_command(linear_x, linear_y, angular_z)  # Send stop command
                     if self.distances['front']['static'] > 1.0 and self.distances['Northwest']['static'] > 1.0:
+                        self.get_logger().info("NO Obstacle F and NW detected !")
                         return  # Exit the function to prevent further movement
-                
+                    
                 elif self.distances['left']['static'] < 1.0 and self.distances['right']['static'] < 1.0:
                     self.get_logger().info("Obstacle detected in robot! Stopping the robot.")
                     linear_x = 0.0
                     linear_y = 0.0
                     angular_z = 0.0
                     self.send_command(linear_x, linear_y, angular_z)  # Send stop command
-                    return  # Exit the function to prevent further movement
+                    
                 else:
                     linear_x = 0.0
                     linear_y = 0.0
