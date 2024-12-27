7bd7236a75adddd96d7b264e9f0b12db4a4e52d5
Duy <108132255+ThaiThanhDuy@users.noreply.github.com>
2024-12-27 13:15:51 +0700

    Update transform_listener_node.py

--- node/node/transform_listener_node.py
+++ node/node/transform_listener_node.py
@@ -166,7 +166,7 @@ class TransformListenerNode(Node):
                         if self.distances['front']['static'] > 1.0 and self.distances['Northeast']['static'] > 1.0:
                             self.get_logger().info("NO Obstacle F and NE detected !")
                             self.obstacle='NO'
-                    return
+                        return
 		
                   
                 if self.distances['right']['static'] < 1.5:
@@ -180,7 +180,7 @@ class TransformListenerNode(Node):
                         if self.distances['front']['static'] > 1.0 and self.distances['Northwest']['static'] > 1.0:
                             self.get_logger().info("NO Obstacle F and NW detected !")
                             self.obstacle='NO'
-                    return
+                        return
 
                     
                     
@@ -194,7 +194,7 @@ class TransformListenerNode(Node):
                         if self.distances['left']['static'] > 1.0 or self.distances['right']['static'] > 1.0:
                             self.get_logger().info("NO Obstacle L and R detected !")
                             self.obstacle='NO'
-                    return
+                        return
                     
                
             else:	
